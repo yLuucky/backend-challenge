8374891599c2991b4c771a9ec937d6e3e2c8d6eb
Lucas Silveira Cardoso <57505844+yLuucky@users.noreply.github.com>
2024-05-13 23:58:20 -0300

    feat: created fromView method

--- source/backend/challenge/modules/task/dtos/TaskProgressDTO.java
+++ source/backend/challenge/modules/task/dtos/TaskProgressDTO.java
@@ -1,5 +1,6 @@
 package backend.challenge.modules.task.dtos;
 
+import backend.challenge.modules.task.infra.http.views.TaskProgressView;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -13,5 +14,13 @@ public class TaskProgressDTO {
 
 	private Long id;
 	private int progress;
+
+	public static TaskProgressDTO fromView(final TaskProgressView taskProgressView, final Long taskId) {
+		final TaskProgressDTO taskProgress = new TaskProgressDTO();
+		taskProgress.setId(taskId);
+		taskProgress.setProgress(taskProgressView.getProgress());
+
+		return taskProgress;
+	}
 
 }
