d7f4f009e64375afb38655f595d6b5b02f37b95b
Lucas Silveira Cardoso <57505844+yLuucky@users.noreply.github.com>
2024-05-13 23:59:15 -0300

    feat: created RetrieveTaskByIdService

---
+++ source/backend/challenge/modules/task/services/RetrieveTaskByIdService.java
@@ -0,0 +1,23 @@
+package backend.challenge.modules.task.services;
+
+import backend.challenge.modules.task.models.Task;
+import backend.challenge.modules.task.repositories.ITaskRepository;
+
+import javax.inject.Inject;
+import javax.inject.Singleton;
+
+@Singleton
+public class RetrieveTaskByIdService implements IRetrieveTaskByIdService{
+
+    private final ITaskRepository taskRepository;
+
+    @Inject
+    public RetrieveTaskByIdService(final ITaskRepository taskRepository) {
+        this.taskRepository = taskRepository;
+    }
+
+    @Override
+    public Task execute(final Long taskId) {
+        return taskRepository.index(taskId);
+    }
+}
