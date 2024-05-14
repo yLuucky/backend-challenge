62a3b5b09b11eecb9b6671818d5e4a05ee6099bb
Lucas Silveira Cardoso <57505844+yLuucky@users.noreply.github.com>
2024-05-14 00:02:53 -0300

    test: created test for RetrieveTaskByIdService

--- tests/backend/challenge/modules/task/services/RetrieveTaskByIdServiceTest.java
+++ tests/backend/challenge/modules/task/services/RetrieveTaskByIdServiceTest.java
@@ -1,6 +1,8 @@
 package backend.challenge.modules.task.services;
 
 
+import backend.challenge.modules.task.dtos.TaskDTO;
+import backend.challenge.modules.task.models.Task;
 import backend.challenge.modules.task.repositories.ITaskRepository;
 import backend.challenge.modules.task.repositories.TaskRepository;
 import kikaha.core.test.KikahaRunner;
@@ -8,10 +10,22 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
 @RunWith( KikahaRunner.class )
 public class RetrieveTaskByIdServiceTest {
 
 	private IRetrieveTaskByIdService retrieveTaskByIdService;
+	private ICreateTaskService createTaskService;
+
+	@Before
+	public void init () {
+		final ITaskRepository taskRepository = new TaskRepository();
+
+		retrieveTaskByIdService = new RetrieveTaskByIdService(taskRepository);
+		createTaskService = new CreateTaskService(taskRepository);
+	}
 
 	@Test
 	public void shouldBeAbleToListTheTaskById() {
@@ -19,6 +33,13 @@ public class RetrieveTaskByIdServiceTest {
 			TODO: Para que esse teste passe, sua aplicação deve permitir que seja
 			 			retornado uma tarefa com o mesmo id informado.
 		*/
+		 TaskDTO taskDTO = Utils.newTaskDTO();
+		 Task newTask = createTaskService.execute(taskDTO);
+
+		 Task taskById = retrieveTaskByIdService.execute(newTask.getId());
+
+		 assertNotNull(newTask);
+		 assertEquals(taskById.getId(), newTask.getId());
 	}
 
 }
