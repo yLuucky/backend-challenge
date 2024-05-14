85df347cc226a4827739cd7f520a25996f869f21
Lucas Silveira Cardoso <57505844+yLuucky@users.noreply.github.com>
2024-05-13 23:58:10 -0300

    feat: created fromView method

--- source/backend/challenge/modules/task/dtos/TaskDTO.java
+++ source/backend/challenge/modules/task/dtos/TaskDTO.java
@@ -1,12 +1,10 @@
 package backend.challenge.modules.task.dtos;
 
-import backend.challenge.modules.task.enums.TaskStatus;
+import backend.challenge.modules.task.infra.http.views.TaskView;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.experimental.Accessors;
-
-import java.util.Date;
 
 @Getter
 @Setter
@@ -16,5 +14,13 @@ public class TaskDTO {
 
 	private String title;
 	private String description;
+
+	public static TaskDTO fromView(final TaskView taskView) {
+		final TaskDTO task = new TaskDTO();
+		task.setTitle(taskView.getTitle());
+		task.setDescription(taskView.getDescription());
+
+		return task;
+	}
 
 }
