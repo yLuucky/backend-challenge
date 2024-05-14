d856be084c5f2145d3f5c7f692eb0a0ba0e19397
Lucas Silveira Cardoso <57505844+yLuucky@users.noreply.github.com>
2024-05-13 23:57:03 -0300

    feat: implemented DeleteTaskService

--- source/backend/challenge/modules/task/services/DeleteTaskService.java
+++ source/backend/challenge/modules/task/services/DeleteTaskService.java
@@ -1,23 +1,34 @@
 package backend.challenge.modules.task.services;
 
+import backend.challenge.modules.task.models.Task;
 import backend.challenge.modules.task.repositories.ITaskRepository;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
 
+import static java.util.Objects.isNull;
+
 @Singleton
 public class DeleteTaskService implements IDeleteTaskService {
 
 	private final ITaskRepository taskRepository;
+	private final IRetrieveTaskByIdService retrieveTaskByIdService;
 
 	@Inject
-	public DeleteTaskService(final ITaskRepository taskRepository) {
+	public DeleteTaskService(final ITaskRepository taskRepository,
+							 final IRetrieveTaskByIdService retrieveTaskByIdService) {
 		this.taskRepository = taskRepository;
-	}
+        this.retrieveTaskByIdService = retrieveTaskByIdService;
+    }
 
 	@Override
-	public void execute(Long taskId) {
-		// TODO: Criar serviço responsável por deletar a tarefa
+	public void execute(final Long taskId) {
+		Task deleteTask = retrieveTaskByIdService.execute(taskId);
+		if (isNull(deleteTask)) {
+			return;
+		}
+
+		taskRepository.delete(taskId);
 	}
 
 }
