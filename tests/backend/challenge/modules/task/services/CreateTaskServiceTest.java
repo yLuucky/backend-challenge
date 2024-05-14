fca138747d0a6442f40390cac6c2cdb17b469415
Lucas Silveira Cardoso <57505844+yLuucky@users.noreply.github.com>
2024-05-14 00:02:05 -0300

    test: created test for createTaskService

--- tests/backend/challenge/modules/task/services/CreateTaskServiceTest.java
+++ tests/backend/challenge/modules/task/services/CreateTaskServiceTest.java
@@ -1,29 +1,39 @@
 package backend.challenge.modules.task.services;
 
+import backend.challenge.modules.task.dtos.TaskDTO;
+import backend.challenge.modules.task.models.Task;
 import backend.challenge.modules.task.repositories.ITaskRepository;
 import backend.challenge.modules.task.repositories.TaskRepository;
 import kikaha.core.test.KikahaRunner;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 @RunWith( KikahaRunner.class )
 public class CreateTaskServiceTest {
 
 	private ICreateTaskService createTaskService;
+	private IRetrieveAllTasksService retrieveAllTasksService;
 
 	@Before
 	public void init() {
 		final ITaskRepository taskRepository = new TaskRepository();
 
 		createTaskService = new CreateTaskService(taskRepository);
+		retrieveAllTasksService = new RetrieveAllTasksService(taskRepository);
 	}
 
 	@Test
 	public void shouldBeAbleToCreateANewTask() {
-		// TODO:  Para que esse teste passe, sua aplicação deve permitir que
-		//  uma tarefa seja criado, e retorne um json com a tarefa criada.
+		final TaskDTO taskDTO = Utils.newTaskDTO();
+		final Task newTask = createTaskService.execute(taskDTO);
+		retrieveAllTasksService.execute();
+
+		assertNotNull(newTask);
+		assertEquals(taskDTO.getTitle(), newTask.getTitle());
 	}
 
 
