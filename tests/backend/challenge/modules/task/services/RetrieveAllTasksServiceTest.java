8b5b5c6a9fe9b7268c5790d40fd3dba98e0626d4
Lucas Silveira Cardoso <57505844+yLuucky@users.noreply.github.com>
2024-05-14 00:02:40 -0300

    test: created test for RetrieveAllTasksService

--- tests/backend/challenge/modules/task/services/RetrieveAllTasksServiceTest.java
+++ tests/backend/challenge/modules/task/services/RetrieveAllTasksServiceTest.java
@@ -1,23 +1,31 @@
 package backend.challenge.modules.task.services;
 
+import backend.challenge.modules.task.dtos.TaskDTO;
+import backend.challenge.modules.task.models.Task;
 import backend.challenge.modules.task.repositories.ITaskRepository;
 import backend.challenge.modules.task.repositories.TaskRepository;
 import kikaha.core.test.KikahaRunner;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import java.util.List;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
 
 @RunWith( KikahaRunner.class )
 public class RetrieveAllTasksServiceTest {
 
 	private IRetrieveAllTasksService retrieveAllTasksService;
+	private ICreateTaskService createTaskService;
 
 	@Before
 	public void init() {
 		final ITaskRepository taskRepository = new TaskRepository();
 
 		retrieveAllTasksService = new RetrieveAllTasksService(taskRepository);
+		createTaskService = new CreateTaskService(taskRepository);
 	}
 
 	@Test
@@ -26,6 +34,12 @@ public class RetrieveAllTasksServiceTest {
 			TODO: Para que esse teste passe, sua aplicação deve permitir que seja
 					  retornado um array com todas as tarefas que foram criadas até o momento.
 		*/
+		 TaskDTO taskDTO = Utils.newTaskDTO();
+		 createTaskService.execute(taskDTO);
+		 List<Task> tasks = retrieveAllTasksService.execute();
+
+		assertFalse(tasks.isEmpty());
+		assertNotNull(tasks.get(0));
 	}
 
 }
