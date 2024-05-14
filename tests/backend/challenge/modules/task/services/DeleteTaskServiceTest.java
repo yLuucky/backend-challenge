e6a0bfc79c4ba2772dfc99352d45ebe173e3a087
Lucas Silveira Cardoso <57505844+yLuucky@users.noreply.github.com>
2024-05-14 00:02:23 -0300

    test: created test for DeleteTaskService

--- tests/backend/challenge/modules/task/services/DeleteTaskServiceTest.java
+++ tests/backend/challenge/modules/task/services/DeleteTaskServiceTest.java
@@ -1,28 +1,41 @@
 package backend.challenge.modules.task.services;
 
+import backend.challenge.modules.task.dtos.TaskDTO;
+import backend.challenge.modules.task.models.Task;
 import backend.challenge.modules.task.repositories.ITaskRepository;
 import backend.challenge.modules.task.repositories.TaskRepository;
 import kikaha.core.test.KikahaRunner;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import static org.junit.Assert.assertNull;
 
 @RunWith( KikahaRunner.class )
 public class DeleteTaskServiceTest {
 
 	private IDeleteTaskService deleteTaskService;
+	private ICreateTaskService createTaskService;
+	private IRetrieveTaskByIdService retrieveTaskByIdService;
 
 	@Before
 	public void init() {
 		final ITaskRepository taskRepository = new TaskRepository();
 
-		deleteTaskService = new DeleteTaskService(taskRepository);
+		retrieveTaskByIdService = new RetrieveTaskByIdService(taskRepository);
+		deleteTaskService = new DeleteTaskService(taskRepository, retrieveTaskByIdService);
+		createTaskService = new CreateTaskService(taskRepository);
 	}
 
 	@Test
 	public void shouldBeAbleToDeleteTaskById() {
 		// TODO: Para que esse teste passe, sua aplicação deve permitir que tarefas sejam deletadas por id.
+		final TaskDTO taskDTO = Utils.newTaskDTO();
+		final Task newTask = createTaskService.execute(taskDTO);
+
+		deleteTaskService.execute(newTask.getId());
+		final Task task = retrieveTaskByIdService.execute(newTask.getId());
+		assertNull(task);
 	}
 
 
